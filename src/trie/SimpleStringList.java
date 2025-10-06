3d2c551b4a1523aeba3a39fdd6ddda0e100a1bfb
Gabriel Muraro <gabriel.muraro@starya.ai>
2025-10-05 21:37:44 -0300

    Refactor SimpleStringList to manage capacity dynamically during string addition

--- src/trie/SimpleStringList.java
+++ src/trie/SimpleStringList.java
@@ -3,19 +3,24 @@ package trie;
 public class SimpleStringList {
     private String[] data;
     private int size;
+    private int capacity;
 
     public SimpleStringList(int initialCapacity) {
         if (initialCapacity <= 0) initialCapacity = 4;
         data = new String[initialCapacity];
         size = 0;
+        capacity = initialCapacity;
     }
 
     public void add(String s) {
-        if (size == data.length) {
-            String[] novo = new String[data.length * 2];
-            System.arraycopy(data, 0, novo, 0, data.length);
-            data = novo;
-        }
+        if (size == capacity) {
+    String[] novo = new String[capacity * 2];
+    for (int i = 0; i < capacity; i++) {
+        novo[i] = data[i];
+    }
+    data = novo;
+    capacity = capacity * 2;
+}
         data[size++] = s;
     }
 
